e7e3cc6d2233b4c6400ef0fd2bb60126041700f8
Myles Metzler <mmetzler@lan.espec.com>
2017-04-11 16:42:26 -0400

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='chamberconnectlibrary',
-    version='2.0.0',
+    version='2.0.1',
     description='A library for interfacing with Espec North America chambers',
     long_description=readme(),
     url='https://github.com/EspecNorthAmerica/ChamberConnectLibrary',
