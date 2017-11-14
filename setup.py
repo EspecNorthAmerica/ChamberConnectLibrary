0c404afff1648f0dfae89b882ea7e538b9d8abe4
Myles Metzler <mmetzler@lan.espec.com>
2017-11-14 12:48:53 -0500

    Correct pypi package file naming again...

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='chamberconnectlibrary',
-    version='2.1.3',
+    version='2.1.4',
     description='A library for interfacing with Espec North America chambers',
     long_description=readme(),
     url='https://github.com/EspecNorthAmerica/ChamberConnectLibrary',
